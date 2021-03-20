e3e5d5e056b4271c5dadf19262ef39dd85c5d836
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2021-03-20 11:38:07 +0100

    map option

---
+++ src/type/MapOption.ts
@@ -0,0 +1,12 @@
+import { none, Option, some } from "@app/type/Option";
+
+// type
+export type MapOption = <A, B>(self: Option<A>, f: (a: A) => B) => Option<B>;
+
+// type constructor
+export const mapOption: MapOption = (self, f) => {
+  if (self._tag === "Some") {
+    return some(f(self.value));
+  }
+  return none;
+};
