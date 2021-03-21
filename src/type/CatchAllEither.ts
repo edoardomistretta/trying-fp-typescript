bd70b8193d29095ec59d98a5d6478f8e3414ca5d
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2021-03-21 10:40:00 +0100

    catch all either

--- src/type/CatchAllEither.ts
+++ src/type/CatchAllEither.ts
@@ -1,15 +1,15 @@
 import { Either } from "@app/type/Either";
 
 // type
-type CatchAllEither = <E, E1, A>(
+export type CatchAllEither = <E, E1, A>(
   self: Either<E, A>,
   f: (a: E) => Either<E1, A>
 ) => Either<E1, A>;
 
 // type constructor
 export const catchAllEither: CatchAllEither = (self, f) => {
-  if (self._tag === "Right") {
-    return self;
+  if (self._tag === "Left") {
+    return f(self.left);
   }
-  return f(self.left);
+  return self;
 };
